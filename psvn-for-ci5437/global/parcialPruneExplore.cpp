021cbc7adda76b2f644fb37f6d7b46c6359ea6a3
David <davidhervil@gmail.com>
2018-02-22 15:52:20 -0400

    Signal handler

--- psvn-for-ci5437/global/parcialPruneExplore.cpp
+++ psvn-for-ci5437/global/parcialPruneExplore.cpp
@@ -6,6 +6,14 @@
 #include <queue>
 #include "priority_queue.hpp"
 
+FILE *file;
+
+void death_handler(int signal){
+    printf("DYING\n");
+    fclose(file);
+    exit(signal);
+}
+
 long long bounded_dfs_visit (state_t state, long long d, long long bound, int hist){
     int ruleid, child_hist;
     long long nodes=0;
@@ -33,10 +41,17 @@ int main(int argc, char **argv) {
     long long nodesAtD, bound;
     ruleid_iterator_t iter;
 
+    signal(SIGKILL, death_handler);
+    signal(SIGINT, death_handler);
+    signal(SIGTERM, death_handler);
+
     //PriorityQueue<state_t> open; // used for the states we have generated but not yet expanded (the OPEN list)
     //std::queue<std::pair<state_t,int> > la_queue;
     //state_map_t *map = new_state_map(); // contains the cost-to-goal for all states that have been generated
-    FILE *file; // the final state_map is written to this file if it is provided (command line argument)
+    file = fopen(argv[1], "w"); // the final state_map is written to this file if it is provided (command line argument)
+    if (file==NULL){
+        file = stdout;
+    }
 
     // add goal states
     first_goal_state(&state, &goalIter);
@@ -47,7 +62,7 @@ int main(int argc, char **argv) {
         
         hist = init_history;        
         nodesAtD = bounded_dfs_visit(state, 0, bound, hist);
-        printf("%lld nodes at depth %lld\n", nodesAtD, bound);
+        fprintf(file, "%lld nodes at depth %lld\n", nodesAtD, bound);
         bound++;
         
     }
