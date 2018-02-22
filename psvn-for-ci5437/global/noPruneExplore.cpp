72cf3436215da362b12e9a6167a51f376d5be987
David <davidhervil@gmail.com>
2018-02-22 12:03:01 -0400

    Signal handler for timeout runs

--- psvn-for-ci5437/global/noPruneExplore.cpp
+++ psvn-for-ci5437/global/noPruneExplore.cpp
@@ -4,7 +4,16 @@
 
 #include <vector>
 #include <queue>
+#include <csignal>
 #include "priority_queue.hpp"
+
+FILE *file;
+
+void death_handler(int signal){
+    printf("DYING\n");
+    fclose(file);
+    exit(signal);
+}
 
 long long bounded_dfs_visit (state_t state, long long d, long long bound){
     int ruleid;
@@ -30,11 +39,17 @@ int main(int argc, char **argv) {
     long long nodesAtD, bound;
     ruleid_iterator_t iter;
 
+    signal(SIGKILL, death_handler);
+    signal(SIGINT, death_handler);
+    signal(SIGTERM, death_handler);
+    
     //PriorityQueue<state_t> open; // used for the states we have generated but not yet expanded (the OPEN list)
     //std::queue<std::pair<state_t,int> > la_queue;
     //state_map_t *map = new_state_map(); // contains the cost-to-goal for all states that have been generated
-    FILE *file; // the final state_map is written to this file if it is provided (command line argument)
-
+    file = fopen(argv[1], "w"); // the final state_map is written to this file if it is provided (command line argument)
+    if (file==NULL){
+        file = stdout;
+    }
     // add goal states
     first_goal_state(&state, &goalIter);
 
@@ -43,7 +58,7 @@ int main(int argc, char **argv) {
     while( true ) {
         
         nodesAtD = bounded_dfs_visit(state, 0, bound);
-        printf("%lld nodes at depth %lld\n", nodesAtD, bound);
+        fprintf(file, "%lld nodes at depth %lld\n", nodesAtD, bound);
         bound++;
         
     }
