bb84fdacbf1dfa3502076c7867708a5e2aa242ba
arturo1212 <arturoantoniotr@gmail.com>
2018-02-21 21:37:13 -0400

    pdbs, falta rubik

--- psvn-for-ci5437/global/ida.cpp
+++ psvn-for-ci5437/global/ida.cpp
@@ -13,6 +13,48 @@ char str[MAX_LINE_LENGTH + 1];
 ruleid_iterator_t iter;
 int history = init_history;
 
+
+
+unsigned int h_value(){
+    state_t abst_state;
+    abstract_state(abst, &state, &abst_state);
+    const int *pdb_value = state_map_get(pdb, &abst_state);
+    assert(pdb_value != 0);
+    return *pdb_value;
+}
+
+std::pair<bool,unsigned int> dfs_ida(unsigned int bound,unsigned int g)
+{
+    // Obtener valor del pdb (Heuristica)
+    unsigned int h = h_value();
+    unsigned int f = g + h; // Costo con heuristica.
+    //printf("holas");
+    if(f>bound) return std::pair<unsigned int, unsigned int>(false, f);
+    if(h==0) return std::pair<unsigned int, unsigned int>(true, g);
+
+    unsigned int t = UINT_MAX;
+    init_fwd_iter(&iter, &state);
+    while( (ruleid = next_ruleid(&iter) ) >= 0 ) {
+        //printf("holiwis");
+        apply_fwd_rule(ruleid, &state, &child);
+        const int cost = g + get_fwd_rule_cost(ruleid);
+        state = child;
+        if(h_value() < t)
+        {
+            history = next_fwd_history(int history, int ruleid);
+            std::pair<bool, unsigned int> p = dfs_ida(bound, cost);
+            if(p.first) return p;
+            t = std::min(t, p.second);
+        }
+        history = next_bwd_history(int history, int ruleid);
+        apply_bwd_rule(ruleid, &state, &child);
+        state = child;
+    }
+    return std::pair<unsigned int, unsigned int>(false, t);
+}
+
+
+
 int main(int argc, char **argv) {
     // read args
     if( argc < 2 ) {
@@ -40,41 +82,13 @@ int main(int argc, char **argv) {
     FILE *pdb_file = fopen(pdb_fname, "r");          // Abrir y Leer PDB
     pdb = read_state_map(pdb_file);
     fclose(pdb_file);
-}
 
-unsigned int h_value(){
-    state_t abst_state;
-    abstract_state(abst, &state, &abst_state);
-    const int *pdb_value = state_map_get(pdb, &abst_state);
-    assert(pdb_value != 0);
-    return *pdb_value;
-}
-
-std::pair<bool,unsigned int> dfs_ida(unsigned int bound,unsigned int g)
-{
-    // Obtener valor del pdb (Heuristica)
-    unsigned int h = h_value();
-    unsigned int f = g + h; // Costo con heuristica.
-    if(f>bound) return std::pair<unsigned int, unsigned int>(false, f);
-    if(h==0) return std::pair<unsigned int, unsigned int>(true, g);
-
-    unsigned int t = UINT_MAX;
-    init_fwd_iter(&iter, &state);
-    while( (ruleid = next_ruleid(&iter) ) >= 0 ) {
-        apply_fwd_rule(ruleid, &state, &child);
-        const int cost = g + get_fwd_rule_cost(ruleid);
-        state = child;
-        if(h_value() < t)
-        {
-            history = next_fwd_history(int history, int ruleid);
-            std::pair<bool, unsigned int> p = dfs_ida(bound, cost);
-            if(p.first) return p;
-            t = std::min(t, p.second);
-        }
-        history = next_bwd_history(int history, int ruleid);
-        apply_bwd_rule(ruleid, &state, &child);
-        state = child;
+    int bound = h_value();
+    while(true){
+        std::pair<bool, unsigned int> p = dfs_ida(bound, 0);
+        if(p.first); return 0;
+        bound = p.second;
     }
-    return std::pair<unsigned int, unsigned int>(false, t);
 }
+
 
