b1ba06c0117ae1e63e1dd522edd158ca33ca30d1
GIA <grupogiausb@gmail.com>
2018-02-23 06:03:11 -0400

    results

---
+++ Arboles_Busqueda/Hanoi/runIda.sh
@@ -0,0 +1,3 @@
+./4_12Disks/hanoi4_12d.ida 600 4_12Disks/pdbFile1.txt < 4_12Disks/testHanoi12.txt
+./4_14Disks/hanoi4_14d.ida 600 4_14Disks/pdbFile1.txt < 4_14Disks/testHanoi14.txt  
+./4_18Disks/hanoi4_18d.ida 600 4_18Disks/pdbFile1.txt < 4_18Disks/testHanoi18.txt 
